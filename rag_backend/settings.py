dda7d55825aa08d48e35be8fdc506137f7b5251b
VEGAPUNkkk <163754419+VEGAPUNkkk@users.noreply.github.com>
2026-01-22 00:26:26 +0530

    fixed CORS issue

--- rag_backend/settings.py
+++ rag_backend/settings.py
@@ -29,9 +29,9 @@ CORS_ALLOW_METHODS = ["GET", "POST", "OPTIONS"]
 SECRET_KEY = 'django-insecure-v3&1rb3cao7*st*6yv(r*ph)yo6!f3m3gix)35iz35)9^)^tr5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['rag-prod-backend.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,7 +55,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'rag_backend.urls'
