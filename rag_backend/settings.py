669583593a73e0c7c8e943e851e5c29c2d87b4dc
VEGAPUNkkk <163754419+VEGAPUNkkk@users.noreply.github.com>
2026-01-26 16:52:53 +0530

    updated settings.py

--- rag_backend/settings.py
+++ rag_backend/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +28,7 @@ CORS_ALLOW_METHODS = ["GET", "POST", "OPTIONS"]
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v3&1rb3cao7*st*6yv(r*ph)yo6!f3m3gix)35iz35)9^)^tr5'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
